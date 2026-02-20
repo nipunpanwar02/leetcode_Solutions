55bb7abf2c0d984c99b60d3c40c03b72cf3774ff
Nipun Panwar <panwarnipun02@gmail.com>
2026-02-20 15:38:46 +0530

    Time: 0 ms (100%), Space: 42.8 MB (98.15%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,18 +1,50 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
+        
+        if(word.length() == 1)return true ;
 
-        if (word.equals(word.toUpperCase())) {
-            return true;
+
+        char chFirst = word.charAt(1);
+        int cap = 0 ; // smal 
+
+        if(chFirst >= 'A' && chFirst <= 'Z'){
+            cap = 1 ;
         }
 
-        if (word.equals(word.toLowerCase())) {
-            return true;
+
+        for(int i = 2 ; i < word.length() ; i++){
+
+            char ch = word.charAt(i) ;
+
+            int curr = 0 ;
+
+            if(ch >= 'A' && ch <= 'Z'){
+                curr = 1;
+            }
+
+            if(curr != cap)return false ; 
+            
+
         }
 
-        if (Character.isUpperCase(word.charAt(0)) &&
-            word.substring(1).equals(word.substring(1).toLowerCase())) {
-            return true;
+        if(cap == 1){
+
+            char c = word.charAt(0);
+
+            if(c >= 'a' && c <= 'z')return false ;
+
         }
-        return false;
+
+        return true ; 
+
+
     }
 }
+
+
+//
+
+
+// small 
+// big  USA  Happy // small  1 - n-1 saab same hona. chaye
+// first big else small 
