f37384bd6ae870900234db380791391940f053ba
Nipun Panwar <panwarnipun02@gmail.com>
2026-03-03 23:08:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -3,9 +3,8 @@ class Solution {
         int n = nums.length;
         int ans[] = new int[2*n];
 
-        for(int i=0;i<n;i++){
-            ans[i]=nums[i];
-            ans[n+i]=nums[i];
+        for(int i=0;i<2*n;i++){
+            ans[i]=nums[i%n];
         }
         return ans;
     }
