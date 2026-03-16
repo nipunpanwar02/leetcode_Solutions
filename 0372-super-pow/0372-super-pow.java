c7c19456b750fa327a441240b38588acb2e94c63
Nipun Panwar <panwarnipun02@gmail.com>
2026-03-16 15:26:21 +0530

    Time: 4 ms (71.04%), Space: 46.5 MB (21.39%) - LeetHub

--- 0372-super-pow/0372-super-pow.java
+++ 0372-super-pow/0372-super-pow.java
@@ -3,6 +3,7 @@ class Solution {
     public int superPow(int a, int[] b) {
         int mod = 1337;
         int result = 1;
+        a = a % mod;
 
         for(int digit=0;digit<b.length;digit++){
             result = (power(result,10,mod) * power(a,b[digit],mod)) % mod;
@@ -12,6 +13,7 @@ class Solution {
 
     public int power(int base, int exp, int mod){
         int result = 1;
+        base = base % mod;
 
         while(exp > 0){
 
