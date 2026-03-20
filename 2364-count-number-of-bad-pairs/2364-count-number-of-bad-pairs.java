4a3f8f094b5fd1b6e51a089a9981f592385ce8b9
Nipun Panwar <panwarnipun02@gmail.com>
2026-03-20 08:31:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.java
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.java
@@ -1,19 +1,40 @@
+// class Solution {
+//     public long countBadPairs(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+
+//         long count = 0;
+//         for(int i=0; i<nums.length;i++){
+//             int good = (nums[i] - i);
+
+//             if(map.containsKey(good)){
+//                 //count += total till that index - good pairs till that index//
+//                 count += i - map.get(good);
+//                 map.put(good,map.get(good) + 1);
+//             }
+//             else{
+//                 count += i;
+//                  map.put(good, 1);
+//             }
+//         }
+//         return count;
+//     }
+// }
+//if we use if-else then there is 2 lookups in hashmap but if we use getOrDefault then it is only 1 lookup and also getOrDefault is more interview friendly then we have to use it instead..//
+
+
+
 class Solution {
-    public long countBadPairs(int[] nums) {
+    public long countBadPairs(int[] nums){
         HashMap<Integer,Integer> map = new HashMap<>();
 
         long count = 0;
-        for(int i=0; i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){
             int good = (nums[i] - i);
 
-            if(map.containsKey(good)){
-                count += i - map.get(good);
-                map.put(good,map.get(good) + 1);
-            }
-            else{
-                count += i;
-                 map.put(good, 1);
-            }
+            int same = map.getOrDefault(good,0);
+            count += i - same;
+
+            map.put(good,same+1);
         }
         return count;
     }
