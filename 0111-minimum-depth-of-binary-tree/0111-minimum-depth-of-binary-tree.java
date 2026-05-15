ec4ff3dd5d63b4ba9cab60ea91722c855c534d1d
Nipun Panwar <panwarnipun02@gmail.com>
2026-05-15 23:05:53 +0530

    Time: 3 ms (76.78%), Space: 82.3 MB (11.89%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -16,7 +16,7 @@
 class Solution {
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
-
+        //we write this condition bcoz if there is a null then it automatically takes the other bcoz min depth is shortest dist. btw root and leaf node and in case of null it is not a leafnode thats why we take the other..//
         if(root.left==null) return minDepth(root.right) + 1;
         if(root.right==null) return minDepth(root.left) + 1;
 
