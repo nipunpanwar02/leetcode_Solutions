4ee5cb5a2c9e4256075ff1fb6311e21c5ac6e273
Nipun Panwar <panwarnipun02@gmail.com>
2026-05-17 11:11:32 +0530

    Time: 1 ms (99.01%), Space: 43.4 MB (65.67%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i = 0;i<num.length();i++){
+            if(i%2==0){
+                sum1+=num.charAt(i) - '0';
+            }
+            else{
+                sum2+=num.charAt(i) - '0';
+            }            
+        }
+        return (sum1==sum2);
+    }
+}
