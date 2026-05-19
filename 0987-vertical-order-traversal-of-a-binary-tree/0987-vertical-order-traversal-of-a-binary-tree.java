588a1ed2a17ea006b5dd2461fe9fe19b6d573e6a
Nipun Panwar <panwarnipun02@gmail.com>
2026-05-19 15:15:16 +0530

    Time: 3 ms (94.44%), Space: 44 MB (74.86%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -14,52 +14,40 @@
  * }
  */
 class Solution {
-    static class Tuple{
-        TreeNode node;
-        int vertical;
-        int level;
 
-        Tuple(TreeNode node, int vertical, int level){
-            this.node = node;
-            this.vertical = vertical;
-            this.level = level;
-        }
-    }
-    //this is level order traversing BFS using queue..//
+    TreeMap<Integer , TreeMap<Integer , PriorityQueue<Integer>>> map = new TreeMap<>();
+    
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
+        inorder(root, 0, 0);
 
-        TreeMap<Integer , TreeMap<Integer , PriorityQueue<Integer>>> map = new TreeMap<>();
-
-        Queue<Tuple> q = new LinkedList<>();
-        q.add(new Tuple(root, 0, 0));
-
-        while(!q.isEmpty()){
-            Tuple tuple = q.poll();
-            TreeNode node = tuple.node;
-            int vertical = tuple.vertical;
-            int level = tuple.level;
-
-            if(!map.containsKey(vertical)){
-                map.put(vertical, new TreeMap<>());
-            }
-            if(!map.get(vertical).containsKey(level)){
-                map.get(vertical).put(level, new PriorityQueue<>());
-            }
-            map.get(vertical).get(level).add(node.val);
-
-            if(node.left!=null) q.add(new Tuple(node.left, vertical-1, level+1));
-            if(node.right!=null) q.add(new Tuple(node.right, vertical+1, level+1));
-        }
         for(TreeMap<Integer , PriorityQueue<Integer>> vert : map.values()){
             ans.add(new ArrayList<>());
-            
+
             for(PriorityQueue<Integer> nodeval : vert.values()){
+
                 while(!nodeval.isEmpty()){
-                    ans.get(ans.size() - 1).add(nodeval.poll());
+                    ans.get(ans.size()-1).add(nodeval.poll());
                 }
             }
         }
         return ans;
+
+    }
+
+    void inorder(TreeNode root, int vertical, int level){
+        if(root == null) return;
+
+        inorder(root.left, vertical-1, level+1);
+
+        if(!map.containsKey(vertical)){
+            map.put(vertical, new TreeMap<>());
+        }
+        if(!map.get(vertical).containsKey(level)){
+            map.get(vertical).put(level, new PriorityQueue<>());
+        }
+        map.get(vertical).get(level).add(root.val);
+
+        inorder(root.right, vertical+1, level+1);
     }
 }
