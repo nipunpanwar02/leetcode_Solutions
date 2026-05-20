da36b7dd007c833f888c00b52f094c4093480571
Nipun Panwar <panwarnipun02@gmail.com>
2026-05-20 11:29:31 +0530

    Time: 4 ms (18.17%), Space: 46.4 MB (53.97%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -14,20 +14,39 @@
  * }
  */
 class Solution {
-    int maxlevel = -1;
-    int ans = 0;
-    public int findBottomLeftValue(TreeNode root) {
-        helper(root, 0);
-        return ans;
-    }
-    void helper(TreeNode root, int level){
-        if(root == null) return;
+    static class Tuple {
+        TreeNode node;
+        int level;
 
-        if(level > maxlevel){
-            maxlevel = level;
-            ans = root.val;
+        Tuple(TreeNode node, int level){
+            this.node = node;
+            this.level = level;
         }
-        helper(root.left, level+1);
-        helper(root.right, level+1);
+    }
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<Tuple> q = new LinkedList<>();
+        q.add(new Tuple(root, 0));
+
+        int maxlevel = -1;
+        int ans = 0;
+
+        while(!q.isEmpty()){
+            Tuple tuple = q.poll();
+
+            TreeNode node = tuple.node;
+            int level = tuple.level;
+
+            if(level > maxlevel){
+                maxlevel = level;
+                ans = node.val;
+            }
+            if(node.left!=null){
+                q.add(new Tuple(node.left, level + 1));
+            }
+            if(node.right!=null){
+                q.add(new Tuple(node.right, level + 1));
+            }
+        }
+        return ans;
     }
 }
