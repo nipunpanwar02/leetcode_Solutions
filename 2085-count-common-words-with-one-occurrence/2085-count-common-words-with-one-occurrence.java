8a20d7d7b0a2e775a2d84b64b0032efbb8fa0f23
Nipun Panwar <panwarnipun02@gmail.com>
2026-06-26 14:59:59 +0530

    Time: 7 ms (73.7%), Space: 46.1 MB (90.7%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.java
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.java
@@ -1,28 +1,17 @@
 class Solution {
     public int countWords(String[] words1, String[] words2) {
         HashMap<String,Integer> map1 = new HashMap<>();
-        List<String> list1 = new ArrayList<>();
+        HashMap<String,Integer> map2 = new HashMap<>();
+
         for(String s:words1){
             map1.put(s,map1.getOrDefault(s,0)+1);
         }
-        for(String s:words1){
-            if(map1.get(s)==1){
-                list1.add(s);
-            }
-        }
-        HashMap<String,Integer> map2 = new HashMap<>();
-        List<String> list2 = new ArrayList<>();
         for(String s:words2){
             map2.put(s,map2.getOrDefault(s,0)+1);
         }
-        for(String s:words2){
-            if(map2.get(s)==1){
-                list2.add(s);
-            }
-        }
         int count=0;
-        for(int i=0;i<list2.size();i++){
-            if(list1.contains(list2.get(i))){
+        for(String s:map1.keySet()){
+            if(map1.get(s)==1 && map2.getOrDefault(s,0)==1){ //here we dont write map1.getOrDefault because alreday in for loop we tell (s:map1.keySet) that means the key is 100% persent in map1 so there is no case that key is not present and default->0.//
                 count++;
             }
         }
