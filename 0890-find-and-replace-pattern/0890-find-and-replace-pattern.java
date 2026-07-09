c2f8a7c11a847a46921db58263a3c2d680c22e6f
Nipun Panwar <panwarnipun02@gmail.com>
2026-07-10 03:00:01 +0530

    Time: 2 ms (34.63%), Space: 44.1 MB (44.44%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -1,26 +1,31 @@
 class Solution {
-    public String getSignature(String str){
-        HashMap<Character,Integer> map = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
-        int id = 1;
-
-        for(char ch : str.toCharArray()){
-            if(!map.containsKey(ch)){
-                map.put(ch,id);
-                id++;
-            }
-            sb.append(map.get(ch));
-            sb.append("#");    // if we dont add this then it will take 10 as 2 diff id as 1 and 0 that's why we add # after every id..//
-        }
-        return sb.toString();
-    }
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> ans = new ArrayList<>();
 
-        String patternSignature = getSignature(pattern);
-
         for(String word : words){
-            if(patternSignature.equals(getSignature(word))){
+            HashMap<Character,Character> patterntoword = new HashMap<>();
+            HashMap<Character,Character> wordtopattern = new HashMap<>();
+
+            boolean valid = true;
+
+            for(int i=0;i<pattern.length();i++){
+
+                char p = pattern.charAt(i);
+                char w = word.charAt(i);
+
+                if(!patterntoword.containsKey(p) && !wordtopattern.containsKey(w)){
+                    patterntoword.put(p,w);
+                    wordtopattern.put(w,p);
+                }
+                else{
+                    if(patterntoword.getOrDefault(p,'#')!=w || wordtopattern.getOrDefault(w,'#')!=p){
+                        valid = false;
+                        break;
+                    }
+                }
+                //we are using '#' here bcoz if suppose p is not present then it will show null pointer exception thats why getOrDefault if p is not present then it will return #..//
+            }
+            if(valid){
                 ans.add(word);
             }
         }
