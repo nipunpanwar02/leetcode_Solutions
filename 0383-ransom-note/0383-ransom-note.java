21d96852f009850bf8ab214e86562b7007412312
Nipun Panwar <panwarnipun02@gmail.com>
2026-07-31 02:33:19 +0530

    Time: 14 ms (34.69%), Space: 47.2 MB (20.11%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        char[] magz = magazine.toCharArray(); 
+
+        for(char ch : magz){
+            map.put(ch,map.getOrDefault(ch,0) +1);
+        }
+
+        for(int i =0; i<ransomNote.length(); i++){
+            char ch = ransomNote.charAt(i);
+
+            if(!map.containsKey(ch)){
+                return false;
+            }
+            else{
+                map.put(ch,map.get(ch) -1);
+            }
+            if(map.get(ch)==0){
+                map.remove(ch);
+            }
+        }
+        return true;
+    }
+}
