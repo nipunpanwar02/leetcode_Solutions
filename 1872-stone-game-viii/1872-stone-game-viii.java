2e9e7073709c99536de2927749827966f3ce5227
Nipun Panwar <panwarnipun02@gmail.com>
2026-08-24 17:51:52 +0530

    Time: 4 ms (66.81%), Space: 92 MB (42.55%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+
+        int[] prefix = new int[n];
+        prefix[0] = stones[0];
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1]+stones[i];
+        }
+        //Now we start comparing from last[prefix] to first[prefix] to get the max possible diff..//
+        int ans = prefix[n-1];
+        for(int i=n-2;i>0;i--){
+            int finalans = prefix[i] - ans;
+            ans = Math.max(ans, finalans);
+        }
+        return ans;
+    }
+}
